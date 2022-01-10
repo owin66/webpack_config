6412a66e50e665c0e68427e072c0589b827e4515
owin66 <78900270+owin66@users.noreply.github.com>
2022-01-10 13:27:43 +0200

    rollup app

---
+++ src/modules/app.js
@@ -0,0 +1,23 @@
+import {JSBlock} from "./js-block";
+import {TimerBlock} from "./timer-block";
+import {JS_CREATION_DATE} from "../core/constans/settings";
+
+
+export class App {
+    #jsBlock
+    #timerBlock
+
+    constructor() {
+        this.#jsBlock = new JSBlock();
+        this.#timerBlock = new TimerBlock(JS_CREATION_DATE)
+    }
+
+    run() {
+        const jsBlockHTML = this.#jsBlock.render();
+        const timerBlock = this.#timerBlock.render()
+        document.body.append(jsBlockHTML,timerBlock);
+    }
+}
+
+
+
